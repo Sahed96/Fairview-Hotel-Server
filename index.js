e66f0e28a8d6af4f05160a1ba2bc34f99987fbfd
SAHED KHAN <ninja.sahedkhan@gmail.com>
2024-05-15 16:16:04 +0600

    jwt integration complete

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express =require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const app = express();
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://fairview-hotel-c14d2.web.app/",
+    "https://fairview-hotel-c14d2.firebaseapp.com/",
+  ],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -23,6 +34,37 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyToken = async (req ,res , next) => {
+  const token = req?.cookies?.token
+  console.log('middleware token value', token);
+  
+  if(!token){
+      return res.status(401).send({message: 'invalid access'})
+
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error , decoded) =>{
+      if(error){
+          console.log(error);
+          return res.status(401).send({message: 'unauthorized access'})
+      }
+      console.log('value of token',decoded);
+      req.user = decoded;
+      next()
+  })
+}
+
+// const cookieOptions = {
+//   httpOnly: true,
+//   secure: true,
+//   sameSite: 'none',
+// };
+
+const logger = (req, res, next) => {
+  console.log('log: info',req.method, req.url);
+  next()
+}
+
 async function run() {
   try {
 
@@ -36,9 +78,55 @@ async function run() {
       res.send(result);
   })
     app.get('/allReview', async (req, res) =>{
-      const cursor = reviewCollection.find();
+      const cursor = reviewCollection.find().sort({date: -1});
       const result = await cursor.toArray();
       res.send(result);
+  })
+
+
+  // create token
+  app.post("/jwt", async (req, res) => {
+    const user = req.body;
+    console.log("user for token", user);
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '2h'});
+    res
+    .cookie('token',token,{
+      httpOnly: true,
+      secure:true,
+      sameSite: 'none'
+    })
+    .send({success: true})
+  
+    // res.cookie("token", token).send({ success: true });
+  });
+
+  // remove token
+  app.post("/logout", async (req, res) => {
+    const user = req.body;
+    console.log("logging out", user);
+    res
+      .clearCookie("token", { maxAge: 0 })
+      .send({ success: true });
+  });
+
+  app.get('/detailsReview/:id', async (req,res) => {
+    const id = req.params.id
+    console.log(id);
+    const result= await reviewCollection.findOne({ratingId: id})
+    res.send(result)
+  })
+
+  app.get('/sortedData', async (req,res) => {
+    const query = req.query.value
+    console.log(query);
+    let priceRange = {}
+    if(query === 'low' ) {
+      priceRange = { price: { $lt: 500 } }
+    }if(query === 'high') {
+      priceRange = { price: { $gte: 500} }
+    }
+    const result = await roomCollection.find(priceRange).toArray()
+    res.send(result)
   })
 
   app.get('/allRooms/:id', async (req, res) => {
@@ -77,7 +165,11 @@ app.patch('/unavailability/:id', async (req,res) => {
   res.send(result)
 })
 
-app.get('/bookedRoom', async (req, res) => {
+app.get('/bookedRoom',logger,verifyToken, async (req, res) => {
+  console.log('owner info',req.user);
+  if(req.user.email !== req.query.email){
+    return res.status(403).send({message: 'forbidden access'})
+  }
   let query = {};
   if(req.query?.email){
     query = {email: req.query.email}
