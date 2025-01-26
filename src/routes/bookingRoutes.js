78261f33aaedfa690221e73b3ec4343abae752ed
Sahed96 <ninja.sahedkhan@gmail.com>
2025-01-26 15:13:37 +0600

    room booking route added

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -10,10 +10,10 @@ const verifyToken = require('../middleware/verifyToken');
 const router = express.Router();
 
 // Routes
-router.get('/', verifyToken, getAllBookedRooms); // Get all booked rooms for a user
-router.get('/:id', verifyToken, getBookedRoomDetails); // Get details of a specific booking
-router.post('/', addBookedRoom); // Add a new booking
-router.delete('/:id', deleteBookedRoom); // Delete a booking
+router.get('/bookedRoom', verifyToken, getAllBookedRooms); // Get all booked rooms for a user
+router.get('/bookedRoom/:id', verifyToken, getBookedRoomDetails); // Get details of a specific booking
+router.post('/bookedRoom', addBookedRoom); // Add a new booking
+router.delete('/bookedRoom/:id', deleteBookedRoom); // Delete a booking
 router.patch('/update/:id', updateBookingDates); // Update booking dates
 
 module.exports = router;
